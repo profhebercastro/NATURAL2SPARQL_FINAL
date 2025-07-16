1ab49bd56c6ff747a30dd34950643c37fbe9024a
hebercastro79 <heber.castro@usp.br>
2025-07-16 14:29:23 -0300

    fix(core): Corrige geração de queries para setor e tipo de ação

--- src/main/java/com/example/Program/model/ProcessamentoDetalhadoResposta.java
+++ src/main/java/com/example/Program/model/ProcessamentoDetalhadoResposta.java
@@ -2,13 +2,18 @@ package com.example.Programa_heber.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.List;
+import java.util.Map;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProcessamentoDetalhadoResposta {
 
     private String sparqlQuery;
     private String resposta;
     private String erro;
-    private String templateId; 
+    private String templateId;
+
+    private List<Map<String, String>> resultados;
 
     // Getters e Setters
     public String getSparqlQuery() {
@@ -41,5 +46,13 @@ public class ProcessamentoDetalhadoResposta {
 
     public void setTemplateId(String templateId) {
         this.templateId = templateId;
+    }
+
+    public List<Map<String, String>> getResultados() {
+        return resultados;
+    }
+
+    public void setResultados(List<Map<String, String>> resultados) {
+        this.resultados = resultados;
     }
 }
