b5840b54ce5b841f80d6266d87360de0ede8b3c8
hebercastro79 <heber.castro@usp.br>
2025-07-24 10:02:20 -0300

    fix: Implement final robust entity extraction in NLP controller

--- src/main/java/com/example/Program/model/ExecuteQueryRequest.java
+++ src/main/java/com/example/Program/model/ExecuteQueryRequest.java
@@ -6,24 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class ExecuteQueryRequest {
     
     private String query;
-
-    // Getters e Setters
-    
-    public String getQuery() {
-        return query;
-    }
-
-    public void setQuery(String query) {
-        this.query = query;
-    }
-}package com.example.Program.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class ExecuteQueryRequest {
-    
-    private String query;
     private String tipoMetrica;
 
     // Getters e Setters
